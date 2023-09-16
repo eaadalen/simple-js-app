a762961dca315b88774afb4afef1a496d521d5f5
eaadalen <eaadalen112@gmail.com>
2023-09-16 11:06:23 -0500

    Add IIFE

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-var pokemonList = [{name:"Bulbasaur",height:0.7,type:["grass","poison"]},   //object containing Bulbasaur
-                   {name:"Charmander",height:0.6,type:["fire"]},            //object containing Charmander
-                   {name:"Pikachu",height:0.4,type:["electric"]}];          //object containing Pikachu
-
-function stats_list(item) {
-    document.write(item.name + " (height: " + String(item.height) + ") ");
-    if (item.height > 0.6) {      //check pokemon size
-        document.write("- Wow, that's big!<br>");
+let pokemonRepository = (function () {
+    var pokemonList = [{name:"Bulbasaur",height:0.7,type:["grass","poison"]},   //object containing Bulbasaur
+                       {name:"Charmander",height:0.6,type:["fire"]},            //object containing Charmander
+                       {name:"Pikachu",height:0.4,type:["electric"]}];          //object containing Pikachu
+    return {
+        add: function(pokemon) {
+          pokemonList.push(pokemon);
+        },
+        getAll: function() {
+          return pokemonList;
+        }
     }
-    else {
-        document.write("<br>");
-    }
-}
-
-pokemonList.forEach(stats_list);
+})();
