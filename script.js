8eb07441dc9e47199f04afafe19d542aed1e5f94
eaadalen <eaadalen112@gmail.com>
2023-09-15 21:43:31 -0500

    Pokedex entries

---
+++ script.js
@@ -0,0 +1,3 @@
+var pokemonList = [{name:"Bulbasaur",height:0.7,type:["grass","poison"]},
+                   {name:"Charmander",height:0.6,type:["fire"]},
+                   {name:"Pikachu",height:0.4,type:["electric"]}];
