6377e76529c4f2d29bbf3791b548926cef320139
eaadalen <eaadalen112@gmail.com>
2023-09-16 10:44:45 -0500

    Add foreach loop

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ var pokemonList = [{name:"Bulbasaur",height:0.7,type:["grass","poison"]},   //ob
                    {name:"Charmander",height:0.6,type:["fire"]},            //object containing Charmander
                    {name:"Pikachu",height:0.4,type:["electric"]}];          //object containing Pikachu
 
-for (let i = 0; i < pokemonList.length; i++) {  //Print out pokemon
-    document.write(pokemonList[i].name + " (height: " + String(pokemonList[i].height) + ") ");
-    if (pokemonList[i].height > 0.6) {      //check pokemon size
+function stats_list(item) {
+    document.write(item.name + " (height: " + String(item.height) + ") ");
+    if (item.height > 0.6) {      //check pokemon size
         document.write("- Wow, that's big!<br>");
     }
     else {
         document.write("<br>");
     }
 }
+
+pokemonList.forEach(stats_list);
