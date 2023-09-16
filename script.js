1074c06c91c0587102cea0d8feab0cc0f7442de4
eaadalen <eaadalen112@gmail.com>
2023-09-15 22:03:19 -0500

    Pokemon list

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var pokemonList = [{name:"Bulbasaur",height:0.7,type:["grass","poison"]},
-                   {name:"Charmander",height:0.6,type:["fire"]},
-                   {name:"Pikachu",height:0.4,type:["electric"]}];
+var pokemonList = [{name:"Bulbasaur",height:0.7,type:["grass","poison"]},   //object containing Bulbasaur
+                   {name:"Charmander",height:0.6,type:["fire"]},            //object containing Charmander
+                   {name:"Pikachu",height:0.4,type:["electric"]}];          //object containing Pikachu
+
+for (let i = 0; i < pokemonList.length; i++) {  //Print out pokemon
+    document.write(pokemonList[i].name + " (height: " + String(pokemonList[i].height) + ") ");
+    if (pokemonList[i].height > 0.6) {      //check pokemon size
+        document.write("- Wow, that's big!<br>");
+    }
+    else {
+        document.write("<br>");
+    }
+}
